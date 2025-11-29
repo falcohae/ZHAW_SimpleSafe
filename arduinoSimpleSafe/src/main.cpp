83981f4628ca8b76dd41d8f064f0c6f92e4d9b2f
Falco <falco.haedicke@gmail.com>
2025-11-29 22:59:52 +0100

    fix last push

--- arduinoSimpleSafe/src/main.cpp
+++ arduinoSimpleSafe/src/main.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,9 +8,4 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
